9480475fd43a58868eb824e07a9354958bfcb696
alicakir <alicakir@std.iyte.edu.tr>
2016-12-07 18:59:22 +0300

    test dosyası silindi.

--- test.py
+++
@@ -1,9 +0,0 @@
-def sent_twit(twit):
-    import tweepy
-    import secrets
-    secrets = secrets.load()
-    auth = tweepy.OAuthHandler(secrets['twitter_consumer_key'], secrets['twitter_consumer_secret'])
-    auth.set_access_token(secrets['twitter_access_token'], secrets['twitter_access_token_secret'])
-
-    api = tweepy.API(auth)
-    return api.update_status(twit)
