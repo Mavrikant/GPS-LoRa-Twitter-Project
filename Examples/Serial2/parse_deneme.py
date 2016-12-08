5c9ea49ccdab0657352e9ec4a1a7b2b2acee21cb
M. Serdar Karaman <Mavrikant@users.noreply.github.com>
2016-12-08 18:55:39 +0200

    -duplicate

--- Examples/Serial2/parse_deneme.py
+++ Examples/Serial2/parse_deneme.py
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
 a="ali, anil, serdar"
 b=a.split(',')
 print b
 print b[0]
-=======
-a="ali, anil, serdar"
-b=a.split(',')
-print b
-print b[0]
->>>>>>> origin/master
+print b[1]
+print b[2]
