3bdab6c0e8dbc53fc46bedc0d574f27b19c2ed4c
RasPi <alicakir@std.iyte.edu.tr>
2016-12-15 19:52:44 +0000

    google linki tweet atildi

--- tweet.py
+++ tweet.py
@@ -4,6 +4,7 @@ import time
 
 ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=0)
 print ser
+#print project_library.send_twit("IYTE")
 while 1:
     time.sleep(4)
     try:
@@ -11,7 +12,12 @@ while 1:
         x=ser.readline()
 	print x
         data = x.split(',')
-
-        print  project_library.send_twit( "long=" + data[0] + " lat=" + data[1] + " time=" + data[2] )
+	print data[0]
+	print data[1]
+        print data[2]
+	print type(data[0])
+	print float(data[0])/100
+       # print  project_library.send_twit( "long=" + data[0] + " lat=" + data[1] + " time=" + data[2] )
+	print project_library.send_twit("http://maps.google.com/maps?q=loc:" + str( float(data[0])/100) + "," +str(float(data[1])/100))
     except:
         print('Data could not be read')
